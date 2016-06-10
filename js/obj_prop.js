c3ee1fe45e08970390a9c9a3a8fbec93a7205e84
Andrew Downing <ad510n1@gmail.com>
2016-06-09 20:16:03 -0700

    initial level

---
+++ js/obj_prop.js
@@ -0,0 +1,34 @@
+"use strict"
+
+var imgProp = {
+  "img/asteroid.png": {
+    width: 50,
+    height: 50,
+    baseX: 25,
+    baseY: 25,
+  },
+  "img/clock.png": {
+    width: 50,
+    height: 50,
+    baseX: 25,
+    baseY: 25,
+  },
+  "img/hole.png": {
+    width: 300,
+    height: 300,
+    baseX: 150,
+    baseY: 150,
+  },
+  "img/obj0.png": {
+    width: 40,
+    height: 40,
+    baseX: 20,
+    baseY: 20,
+  },
+  "img/propel.png": {
+    width: 20,
+    height: 20,
+    baseX: 10,
+    baseY: 10,
+  },
+}
