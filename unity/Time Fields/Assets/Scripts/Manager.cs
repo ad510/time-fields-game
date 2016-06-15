5058ccd1c43315648b2ccccc91739f16b87da746
Andrew Downing <ad510n1@gmail.com>
2016-06-14 20:05:13 -0700

    initial unity port

---
+++ unity/Time Fields/Assets/Scripts/Manager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour {
+	public GameObject playerPrefab, asteroidPrefab, fieldPrefab, propelPrefab;
+
+	const float updateRate = 33;
+	const float radius = 2000;
+	const float playerAcc = 0.2f;
+	const float playerRotSpd = Mathf.PI / 1000 * updateRate;
+	const float propelSpd = 1 * updateRate;
+
+	public static float timeScale = 1;
+	public static Obj player;
+	public static List<Obj> asteroids = new List<Obj>();
+	public static List<Obj> fields = new List<Obj>();
+
+	// Use this for initialization
+	void Start() {
+		Time.fixedDeltaTime = updateRate / 1000;
+		player = new Obj(Instantiate(playerPrefab) as GameObject, new Vector2(), new Vector2(), Mathf.PI / 2);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		while (fields.Count < 20) {
+			Vector2 p = RandInsideCircle(1000, 2000);
+			Vector2 v = RandInsideCircle(0, 0.15f * updateRate);
+			if (Random.value < 0.1) {
+				fields.Add(new Obj(Instantiate(fieldPrefab) as GameObject, player.pos + p, player.dilatedVel + v, 0));
+			} else {
+				Obj asteroid = new Obj(Instantiate(asteroidPrefab) as GameObject, player.pos + p, player.dilatedVel + v, Random.value * Mathf.PI * 2);
+				asteroid.velRot = Random.Range(-playerRotSpd * 2, playerRotSpd * 2);
+				asteroids.Add(asteroid);
+			}
+		}
+		player.Draw();
+		for (int i = 0; i < asteroids.Count; i++) {
+			asteroids[i].Draw();
+			if (Vector2.Distance(asteroids[i].pos, player.pos) > radius) {
+				Destroy(asteroids[i].go);
+				asteroids.RemoveAt(i);
+				i--;
+			}
+		}
+		for (int i = 0; i < fields.Count; i++) {
+			fields[i].Draw();
+			if (Vector2.Distance(fields[i].pos, player.pos) > radius) {
+				Destroy(fields[i].go);
+				fields.RemoveAt(i);
+				i--;
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		if (timeScale < updateRate / 1500) return;
+		if (Input.GetKey(KeyCode.UpArrow) && Random.value < 0.02f * updateRate) Propel(-1);
+		if (Input.GetKey(KeyCode.DownArrow) && Random.value < 0.02f * updateRate) Propel(1);
+		foreach (Obj field in fields) field.prevPos = field.pos;
+		foreach (Obj field in fields) field.UpdatePos(0, 0);
+		foreach (Obj asteroid in asteroids) asteroid.UpdatePos(0, asteroid.velRot);
+		timeScale = player.UpdatePos(Input.GetAxis("Vertical") * playerAcc, -Input.GetAxis("Horizontal") * playerRotSpd);
+	}
+
+	void Propel(float dir) {
+		asteroids.Add(new Obj(Instantiate(propelPrefab) as GameObject, player.pos, player.vel + propelSpd * dir * new Vector2(Mathf.Cos(player.rot), Mathf.Sin(player.rot)) + Random.insideUnitCircle * propelSpd / 6, 0));
+	}
+
+	Vector2 RandInsideCircle(float min, float max) {
+		Vector2 ret;
+		do {
+			ret = Random.insideUnitCircle * max;
+		} while (ret.sqrMagnitude < min * min);
+		return ret;
+	}
+}
---
+++ unity/Time Fields/Assets/Scripts/Obj.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obj {
+	public Vector2 pos, prevPos, vel, dilatedVel;
+	public float rot, velRot;
+	public GameObject go;
+
+	public Obj(GameObject go, Vector2 pos, Vector2 vel, float rot) {
+		this.go = go;
+		this.pos = pos;
+		this.vel = vel;
+		this.dilatedVel = vel;
+		this.rot = rot;
+	}
+
+	public float UpdatePos(float fwd, float velRot) {
+		Obj field = null;
+		float dist = 0, mul = 1;
+		// update velocity
+		vel.x += fwd * Mathf.Cos(rot);
+		vel.y += fwd * Mathf.Sin(rot);
+		// find closest field
+		foreach (Obj f in Manager.fields) {
+			if (f != this) {
+				float d = Vector2.Distance(pos, f.prevPos);
+				float m = Mathf.Clamp01(d / 500 - 0.2f); // possible to compute this after loop, or use a weighted average instead (sum up all delta v's won't work b/c 2 close fields would repel player)
+				if (m < 1 && (field == null || d < dist)) {
+					field = f;
+					dist = d;
+					mul = m;
+				}
+			}
+		}
+		// update position and rotation
+		dilatedVel = field == null ? vel : field.dilatedVel + (vel - field.dilatedVel) * mul;
+		pos += dilatedVel / Manager.timeScale;
+		rot += velRot * mul / Manager.timeScale;
+		return mul;
+	}
+
+	public void Draw() {
+		go.transform.position = (pos - Manager.player.pos) / 100;
+		go.transform.rotation = Quaternion.Euler(0, 0, rot * Mathf.Rad2Deg - 90);
+	}
+}
